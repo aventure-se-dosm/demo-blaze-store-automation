8237a97ea68edf41bf03f482692f7cb1cfbde0a0
Marcelo de Oliveira Santos <=>
2023-07-12 17:57:31 -0300

    5th scenario is finished: >>[1] we still need create all random-generated model feature.

---
+++ src/test/java/business/about_us/AboutUsPage.java
@@ -0,0 +1,27 @@
+package business.about_us;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import test.context.TestContext;
+
+public class AboutUsPage {
+
+	AboutUsPage(){
+		PageFactory.initElements(TestContext.getDriver(), this);
+	}
+	@FindBy(xpath = "//a[@data-target='#videoModal']")
+	private WebElement linkAboutUs;
+	@FindBy(xpath = "//video[@id='example-video_html5_api']/..")
+	private WebElement exampleVideo;
+
+	public WebElement getLinkAboutUs() {
+		return linkAboutUs;
+	}
+
+	public WebElement getExampleVideo() {
+		return exampleVideo;
+	}
+
+}
